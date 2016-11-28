5977e17907c8c262511bb4d7aded564aa1bef759
Sjoerd <Sjoerdk@outlook.com>
2016-11-28 14:59:31 +0100

    vtfgbyhnujik,

---
+++ Social/Server/Library/Player.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public class Player
+    {
+        public int UserID;
+        public List<Card> SelectedDeck;
+        int CurrentEnergy;
+        int MaxEnergy;
+
+        public void StartTurn()
+        {
+
+        }
+
+        public void EndTurn()
+        {
+
+        }
+    }
+}
