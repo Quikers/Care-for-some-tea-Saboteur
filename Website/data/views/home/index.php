281819f00df4a2754c63262bf46604f36ecb05e4
Luuk Diederik <flipmil3@hotmail.com>
2016-12-01 16:37:42 +0100

    Website updatettetetetetetetet

--- Website/data/views/home/index.php
+++ Website/data/views/home/index.php
@@ -1,5 +1,5 @@
 <div id="content-body">
     <h1>Welcome, young Saboteur.</h1><br>
     <p>Welcome to Care for some tea, Saboteur?'s website!</p>
-    <p></p>
+    <p style="color: #DDDD00; font-weight: 600;">This website is currently under construction so bear with us for a few more weeks!</p>
 </div>
