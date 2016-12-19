0694c48f3f4b07317c62cca4dad0e1e565082b78
Christiaan Sommer <chris241@hotmail.nl>
2016-12-19 12:15:36 +0100

    sprint 1 finish

---
+++ Game/Assets/Scripts/MainMenu/LoginManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class LoginManager : MonoBehaviour
+{
+    public Text email;
+    public void Login()
+    {
+        Utilities.API.UserbyEmail( email.text );
+
+    }
+}
